6c92941744d0d0e3080190c9ff05323f068c4c17
shohruh973 <abdumannobovshohruh973@gmail.com>
2022-12-28 17:42:04 +0500

    Java home shohruh

---
+++ index.js
@@ -0,0 +1,26 @@
+let name = "Shohruh" //string
+let age = 18 // number
+
+// let variables 
+// const variables
+// var variables
+
+
+//  key             // value
+let isMarried = true / false // boolean
+let box; // undefined
+// let prom = prompt("Yoshingizni kiriting?");   //null
+let a = prompt("Yoshingizni kiritng")// har doim karobkaga solish kerak
+console.log(a);
+
+
+
+//---------------------data type--------------------------------
+
+//number
+//string
+//boolean
+//undefined
+//null
+//symbol
+//begInt
