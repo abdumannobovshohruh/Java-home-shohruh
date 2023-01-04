df19eac54c4f5623d2816bb572926ec9ca14acb1
shohruh973 <abdumannobovshohruh973@gmail.com>
2023-01-04 22:10:21 +0500

    Java home sh

--- index.js
+++ index.js
@@ -24,3 +24,26 @@ console.log(a);
 //null
 //symbol
 //begInt
+
+// ===================================================
+// let name = "Shohruh"
+// let age = 18
+// console.log("mening ismim " + name +" yoshim "  + age + " da ");
+
+
+
+// let a ="5"
+// let b ="5"
+
+
+// console.log(a+b);
+// console.log(a-b)
+// console.log(a/b)
+// console.log(a*b)
+// let a = prompt("Birinchi sonni kiriting")
+// let b = prompt("ikkinchi sonni kiriting")
+// console.log(+a + +b) /* bu birinchi usul, lekin har doim ham to'g'ri emas */
+
+// let a = prompt+("Birinchi sonni kiriting")
+// let b= prompt+("Ikkinchi sonni kiriting")
+// console.log(a+b) /* yoniga plus qo'yish hal qiladi ba'zida */
